f73d39a8d80f93b5180350c1715b21a413cb7655
VISH01AL <g.vish1al@gmail.com>
2023-10-18 22:01:08 -0400

    started first webapp

---
+++ FirstProject-4/src/main/java/com/example/demo/FirstProject4Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProject4Application.class, args);
+		
+	}
+
+}
