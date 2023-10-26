3804f7ddd488828bd6acccdf7110fb07f8f6ddb0
VISH01AL <g.vish1al@gmail.com>
2023-10-25 21:40:16 -0400

    10/25

---
+++ FirstProject-5/src/main/java/com/example/demo/FirstProject5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProject5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProject5Application.class, args);
+	}
+
+}
