87a168872a1fcbcfe1510d0b4e8c0111381cbc62
VISH01AL <g.vish1al@gmail.com>
2023-10-27 15:30:42 -0400

    learning 9

---
+++ FirstProject-9/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+//	@RequestMapping("home")
+//	
+//	public String home() {
+//		return "home";
+//	}
+
+//	@RequestMapping("home")	
+//	public String home(HttpServletRequest req) {
+//		HttpSession hs=req.getSession();
+//		String name=req.getParameter("name");
+//		hs.setAttribute("name",name);
+//		return "home";
+//	}
+	
+//	@RequestMapping("home")	
+//	public String home(String name,int id,String laptop,HttpSession hs) {
+//		hs.setAttribute("name",name);
+//		hs.setAttribute("id",id*12);
+//		hs.setAttribute("laptop",laptop);
+//		System.out.println(name);
+//		return "home";
+//	}
+	@RequestMapping("home")	
+	public ModelAndView home(Alien al) {
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("home");
+		mv.addObject("al", al);
+	
+		System.out.println(al.toString());
+	
+		return mv;
+	}
+}
---
+++ FirstProject-9/src/main/webapp/pages/home.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<!-- ---<p>name is ${name}</p>
+<p>id times 12 is ${id }</p>
+<p>laptop is ${laptop }</p>----- -->
+
+<p>name is ${al.name}</p>
+<p>id times 12 is ${al.id*12 }</p>
+<p>laptop is ${al.laptop }</p>
+</body>
+</html>
