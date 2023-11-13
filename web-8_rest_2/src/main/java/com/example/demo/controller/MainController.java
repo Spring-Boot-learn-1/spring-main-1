3ef54f3a2048fae2fe908d160239806680d8a563
VISH01AL <g.vish1al@gmail.com>
2023-11-12 23:12:52 -0500

    learning web 8

--- web-8_rest_2/src/main/java/com/example/demo/controller/MainController.java
+++ web-8_rest_2/src/main/java/com/example/demo/controller/MainController.java
@@ -1,16 +1,33 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.dao.RestUserRepo;
+import com.example.demo.model.RestUser;
 
 @Controller
 public class MainController {
-	
+	@Autowired
+	RestUserRepo dataobjectru;
 	@RequestMapping("/")
-	@ResponseBody
 	public String home()
 	{
+		
 		return "jsppages/home.jsp";
+	}
+	@RequestMapping("/addUser")
+	public ModelAndView addUser(ModelAndView mv,RestUser ru) {
+		dataobjectru.save(ru);
+		mv.setViewName("jsppages/home.jsp.jsp");
+		return mv;
+	}
+	@RequestMapping("/getUser")
+	@ResponseBody
+	public String getUser() {
+		return "";
 	}
 }
