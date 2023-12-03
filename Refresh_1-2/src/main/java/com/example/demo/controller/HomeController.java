e4df40d5d8862c28d45215fa129b8309b2448be1
VISH01AL <g.vish1al@gmail.com>
2023-12-02 21:43:56 -0500

    refresh 2 updated

--- Refresh_1-2/src/main/java/com/example/demo/controller/HomeController.java
+++ Refresh_1-2/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,34 @@ public RestUser addUser(@RequestBody RestUser ru) {
 	System.out.println(ru.toString());
 	dataobject.save(ru);
 	return ru;
+}
+@PutMapping("/replaceUser")
+public RestUser replaceUser(@RequestBody RestUser ru) {
+	Optional<RestUser> temp=dataobject.findById(ru.getId());
+	if(!temp.isEmpty()) {
+		RestUser curr=temp.get();
+		curr.setId(ru.getId());
+		curr.setName(ru.getName());
+		curr.setPassword(ru.getPassword());
+		curr.setHistexteditor(ru.getHistexteditor());
+		dataobject.save(curr);
+		return curr;
+	}
+	else {
+		dataobject.save(ru);
+	}
+	return ru;
+}
+@DeleteMapping("/deleteUser/{id}")
+public String deleteUser(@PathVariable int id) {
+	Optional<RestUser> temp=dataobject.findById(id);
+	if(temp.isEmpty()) {
+		return "object doest not exist";
+	}
+	else {
+		dataobject.deleteById(id);
+		return "deleted";
+	}
+	
 }
 }
